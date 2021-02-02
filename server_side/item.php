0c3adf23e4f2358a3e7aa04c1b7d30cea966820b
jkenjayev <javohirwebdev@gmail.com>
2021-02-01 20:16:06 -0800

    retyped the code

--- server_side/item.php
+++ server_side/item.php
@@ -1,129 +1,140 @@
-<?php 
-	class Item {
-		public $itemid;
-		public $title;
-		public $description;
-		public $resaleprice;
-		public $winbidder;
-		public $winprice;
-	}
+<?php
+class Item
+{
+    public $itemid;
+    public $title;
+    public $description;
+    public $resaleprice;
+    public $winbidder;
+    public $winprice;
 
-	function __construct($itemid, $title, $description, $resaleprice, $winbidder, $winprice) {
-		$this->itemid = $itemid;
-		$this->title = $title;
-		$this->description = $description;
-		$this->winbidder = $winbidder;
-		$this->winprice = $winprice;
-	}
+    function __construct($itemid, $title, $description, $resaleprice, $winbidder, $winprice)
+    {
+        $this->itemid = $itemid;
+        $this->title = $title;
+        $this->description = $description;
+        $this->resaleprice = $resaleprice;
+        $this->winbidder = $winbidder;
+        $this->winprice = $winprice;
+    }
 
-	function __toString() {
-		$output = "<h2> Item: $this->itemid </h2>".
-				  "<h2> Name: $this->title </h2>\n";
-				  "<h2> Description: $this->description </h2> \n";
-				  "<h2> Resaleprice: $this->resaleprice </h2> \n";
-				  "<h2> Winning bid: $this->winbidder at $this->winprice </h2> \n";
-		return $output;
-	}
+    function __toString()
+    {
+        $output = "<h2> Item: $this->itemid </h2> \n" .
+            "<h2> Name: $this->title </h2> \n" .
+            "<h2> Description: $this->description </h2> \n" .
+            "<h2> Resale price: $this->resaleprice </h2> \n" .
+            "<h2> Winning bid: $this->winbidder at $this->winprice </h2>";
+        return $output;
+    }
 
-	function saveItem() {
-		$connDB = mysqli_connect('localhost', 'root', 'root', 'auctionHelper');
-		// $query = "INSERT INTO items VALUES ($this->itemid, "$this->title", "$this->description", $this->resaleprice, $this->winbidder, $this->winprice)";
-		$queryResult = mysqli_query($connDB, $query);
-		$connDB->close();
+    function saveItem()
+    {
+        $connDB = mysqli_connect('localhost', 'root', 'root', 'auctionHelper');
+        $query = "INSERT INTO items VALUES ($this->itemid, $this->title, 
+                $this->description, 
+                $this->resaleprice,
+                $this->winbidder,
+                $this->winprice)";
+        $queryResult = mysqli_connect($connDB, $query);
+        return $queryResult;
+    }
 
-		return $queryResult;
-	}
+    function updateItem()
+    {
+        $connDB = mysqli_connect('localhost', 'root', 'root', 'auctionHelper');
+        $query = "UPDATE items SET $this->title, 
+                $this->description, 
+                $this->resaleprice, 
+                $this->winbidder, 
+                $this->winprice 
+                WHERE itemid=$this->itemid";
+        $queryResult = mysqli_connect($connDB, $query);
 
-	function updateItem() {
-		$connDB = mysqli_connect('localhost', 'root', 'root', 'auctionHelper');
-		// $query = "UPDATE items SET "$this->title", "$this->description", $this->resaleprice, "$this->winbidder", $this->winprice WHERE itemid=$this->itemid";
-		$queryResult = mysqli_query($connDB, $query);
-		$connDB->close();	
+        return $queryResult;
+    }
 
-		return $queryResult;
-	}
+    function removeItem()
+    {
+        $connDB = mysqli_connect('localhost', 'root', 'root', 'auctionHelper');
+        $query = "DELETE FROM items WHERE itemid=$this->itemid";
+        $queryResult = mysqli_connect($connDB, $query);
+        return $queryResult;
+    }
 
-	function removeItem() {
-		$connDB = mysqli_connect('localhost', 'root', 'root', 'auctionHelper');
-		$query = "DELETE FROM items WHERE itemid=$this->itemid";
-		$queryResult = mysqli_query($connDB, $query);
-		$connDB->close();
+    static function getItems()
+    {
+        $connDB = mysqli_connect('localhost', 'root', 'root', 'auctionHelper');
+        $query = "SELECT * FROM items";
+        $queryResult = mysqli_connect($connDB, $query);
 
-		return $queryResult;
-	}
+        if (mysqli_num_rows($queryResult) > 0) {
+            $items = array();
+            while ($row = mysqli_fetch_assoc($queryResult)) {
+                $item = new Item(
+                    $row['itemid'],
+                    $row['title'],
+                    $row['description'],
+                    $row['resaleprice'],
+                    $row['winbidder'],
+                    $row['winprice']
+                );
 
-	static function getItems() {
-		$connDB = mysqli_connect('localhost', 'root', 'root', 'auctionHelper');
-		$query = "SELECT * FROM items";
-		$queryResult = mysqli_query($connDB, $query);
+                array_push($items, $item);
+            }
 
-		if(mysqli_num_rows($queryResult) > 0) {
-			$items = array();
-			while ($row = $queryResult->fetch_array(MYSQLI_ASSOC)) {
-				$item = new Items($row['itemid'], $row['title'],
-					$row['description'], $row['resaleprice'], $row['winbidder'], $row['winprice']);
-				array_push($items, $item);
-			}
+            return $items;
+        } else {
+            return NULL;
+        }
+    }
 
-			$connDB->close();
-			return $items;
-		} else {
-			$connDB->close();
-			return NULL;
-		}
-	}
+    static function getItemsByBidder($bidderid)
+    {
+        $connDB = mysqli_connect('localhost', 'root', 'root', 'auctionHelper');
+        $query = "SELECT * FROM items WHERE winbidder= $bidderid";
+        $queryResult = mysqli_connect($connDB, $query);
 
-	static function getItemsByBidder($bidderid) {
-		$connDB = mysqli_connect('localhost', 'root', 'root', 'auctionHelper');
-		$query = "SELECT * FROM items WHERE  winbidder=$bidderid";
-		$queryResult = mysqli_query($connDB, $query);
+        if (mysqli_num_rows($queryResult) > 0) {
+            $items = array();
+            while ($row = mysqli_fetch_assoc($queryResult)) {
+                $item = new Item(
+                    $row['itemid'],
+                    $row['title'],
+                    $row['description'],
+                    $row['resaleprice'],
+                    $row['winbidder'],
+                    $row['winprice']
+                );
 
-		if(mysqli_num_rows($queryResult) > 0) {
-			$items = array();
-			while($row = $queryResult->fetch_array(MYSQLI_ASSOC)) {
-					$item = new Item($row['itemid'], $row['title'], $row['description'], $row['resaleprice'], 
-							$row['winbidder'], $row['winprice']);
-					array_push($items, $item);
-			}
+                array_push($items, $item);
+            }
 
-			$connDB->close();
-			return $queryResult;
-		} else {
-			$connDB->close();
-			return NULL;
-		}
-	}
+            return $queryResult;
+        } else {
+            return NULL;
+        }
+    }
 
-	static function findItem($itemid) {
-		$connDB = mysqli_connect('localhost', 'root', 'root', 'auctionHelper');
-		$query = "SELECT * FROM items WHERE itemid= $itemid";
-		$queryResult = mysqli_query($connDB, $query);
-		$row = $result->fetch_array(MYSQLI_ASSOC);
-		if ($row) {
-			$item = new Item($row['itemid'], $row['name'], $row['description'],
-			$row['resaleprice'], $row['winbidder'], $row['winprice']);
-			$db->close();
-			return $item;
-		} else {
-			$db->close();
-			return NULL;
-		}
-	}
- ?>
+    static function findItem($itemid)
+    {
+        $connDB = mysqli_connect('localhost', 'root', 'root', 'auctionHelper');
+        $query = "SELECT * FROM items  WHERE itemid=$itemid";
+        $queryResult = mysqli_connect($connDB, $query);
+        $row = mysqli_fetch_assoc($queryResult);
+        if ($row) {
+            $item = new Item(
+                $row['itemid'],
+                $row['title'],
+                $row['description'],
+                $row['resaleprice'],
+                $row['winbidder'],
+                $row['winprice']
+            );
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            return $item;
+        } else {
+            return NULL;
+        }
+    }
+}
